9bcc7aec3cd38a03a7b9c68beca0487533392649
Hluf Abebe <hlufabebe2015@gmail.com>
2026-01-13 11:14:28 +0300

    refactor: rename DynamicChildren to DynamicChild and update TrieNode structure

--- src/utils/trie.ts
+++ src/utils/trie.ts
@@ -1,7 +1,7 @@
 import { HTTP_METHODS } from "../enums/methods.enum";
 import { RouteHandler } from "../types/route-handler";
 
-interface DynamicChildren {
+interface DynamicChild {
     parameterName: string,
     node: TrieNode,
 }
@@ -9,7 +9,7 @@ interface DynamicChildren {
 class TrieNode {
     handlers: Map<HTTP_METHODS, RouteHandler[]> | null = null;
     staticChildren: Map<string, TrieNode> | null = null;
-    dynamicChildren: DynamicChildren | null = null;
+    dynamicChild: DynamicChild | null = null;
 }
 
 class Trie {
@@ -34,14 +34,14 @@ class Trie {
 
                 currNode = currNode.staticChildren.get(segment)!;
             } else if (isDynamicSegment) {
-                if (!currNode.dynamicChildren?.parameterName) {
-                    currNode.dynamicChildren = {
+                if (!currNode.dynamicChild?.parameterName) {
+                    currNode.dynamicChild = {
                         parameterName: segment.split(":")[1],
                         node: new TrieNode()
                     }
                 }
 
-                currNode = currNode.dynamicChildren.node!
+                currNode = currNode.dynamicChild.node!
             }
 
         }
@@ -53,9 +53,49 @@ class Trie {
         if (!currNode.handlers.get(method)) {
             currNode.handlers.set(method, handlers)
         }
+    }
 
+    public find(url: string, method: HTTP_METHODS) {
+        const segments = url.split("/").filter((segment) => segment.length != 0)
+        const params: Record<string, any> = {};
+
+        let currNode = this.root;
+        for (const segment of segments) {
+            if (currNode.staticChildren?.has(segment)) {
+                currNode = currNode.staticChildren.get(segment)!
+                continue;
+            }
+
+            if (currNode.dynamicChild) {
+                params[currNode.dynamicChild?.parameterName!] = segment;
+                currNode = currNode.dynamicChild.node
+                continue;
+            }
+
+            return null
+        }
+
+        const handlers = currNode.handlers?.get(method);
+        return { handlers, params }
     }
 }
+
+
+const trie = new Trie()
+//trie.insert("//users/profile", HTTP_METHODS.GET, () => { })
+trie.insert("//users/profile", HTTP_METHODS.GET, [() => { }, () => { }])
+trie.insert("//users/", HTTP_METHODS.GET, () => { })
+trie.insert("//users", HTTP_METHODS.POST, () => { })
+// trie.insert("/", HTTP_METHODS.GET, () => { })
+trie.insert("/users/:id/profile/:userId", HTTP_METHODS.GET, () => { })
+trie.insert("/users/:id/profile/:userId", HTTP_METHODS.POST, () => { })
+
+console.dir(trie, { depth: null })
+
+//console.log(trie.find("//users/profile", HTTP_METHODS.GET))
+console.log(trie.find("//users", HTTP_METHODS.GET))
+console.log(trie.find("//users", HTTP_METHODS.POST))
+console.log(trie.find("//users/12345/profile/6789", HTTP_METHODS.POST))
 
 export {
     Trie,

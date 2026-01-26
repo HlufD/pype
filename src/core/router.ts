60dc69e360b6d5143f3b382207be3cf79da682c7
Hluf Abebe <hlufabebe2015@gmail.com>
2026-01-26 16:32:00 +0300

    use the new router with the server

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,29 +1,32 @@
 import { HTTP_METHODS } from "../enums/methods.enum";
 import { RouteHandler } from "../types/route-handler";
 import { Router, Routes } from "../types/router";
-import { Trie } from "./trie";
+import { RouteNode } from "../utils/Trie-Route";
 
 export class PipeRouter implements Router {
-  private routes = new Trie();
+  private routes = new RouteNode({
+    ignoreDuplicateSlashes: false,
+    ignoreTrailingSlash: false,
+  });
 
   get(url: string, routeHandler: RouteHandler): void {
-    this.routes.register(url, HTTP_METHODS.GET, routeHandler);
+    this.routes.register(url, HTTP_METHODS.GET, [routeHandler]);
   }
 
   post(url: string, routeHandler: RouteHandler): void {
-    this.routes.register(url, HTTP_METHODS.POST, routeHandler);
+    this.routes.register(url, HTTP_METHODS.POST, [routeHandler]);
   }
 
   put(url: string, routeHandler: RouteHandler): void {
-    this.routes.register(url, HTTP_METHODS.PUT, routeHandler);
+    this.routes.register(url, HTTP_METHODS.PUT, [routeHandler]);
   }
 
   delete(url: string, routeHandler: RouteHandler): void {
-    this.routes.register(url, HTTP_METHODS.DELETE, routeHandler);
+    this.routes.register(url, HTTP_METHODS.DELETE, [routeHandler]);
   }
 
   patch(url: string, routeHandler: RouteHandler): void {
-    this.routes.register(url, HTTP_METHODS.PATCH, routeHandler);
+    this.routes.register(url, HTTP_METHODS.PATCH, [routeHandler]);
   }
 
   match(method: HTTP_METHODS, path: string) {
--- src/core/server.ts
+++ src/core/server.ts
@@ -1,32 +1,33 @@
-import { createServer, IncomingMessage, Server, ServerResponse } from "node:http"
-import { Response } from "../types/response";
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
 import { PipeRouter } from "./router";
 import { HTTP_METHODS } from "../enums/methods.enum";
 export class PipeServer {
+  private router: PipeRouter;
 
-    private router: PipeRouter;
+  constructor(router: PipeRouter) {
+    this.router = router;
+  }
 
-    constructor(router: PipeRouter) {
-        this.router = router;
+  public listen(port: number, callback?: () => void) {
+    const server = createServer(this.handleRequest.bind(this));
+    server.listen(port, callback);
+  }
+
+  private handleRequest(req: IncomingMessage, res: ServerResponse) {
+    const method = req.method;
+    const path = req.url!;
+    const matchedRoute = this.router.match(
+      method?.toLocaleLowerCase() as HTTP_METHODS,
+      path,
+    );
+    if (matchedRoute) {
+      matchedRoute.handlers.forEach((handler) => {
+        handler(req, res);
+      });
+    } else {
+      res.statusCode = 404;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Not Found");
     }
-
-    public listen(port: number, callback?: () => void) {
-        const server = createServer(this.handleRequest.bind(this))
-        server.listen(port, callback)
-    }
-
-    private handleRequest(req: IncomingMessage, res: ServerResponse) {
-        const method = req.method;
-        const path = req.url!;
-        const matchedRoute = this.router.match(method?.toLocaleLowerCase() as HTTP_METHODS, path);
-        if (matchedRoute) {
-            matchedRoute.handlers.forEach((handler) => {
-                handler(req, res)
-            })
-        } else {
-            res.statusCode = 404;
-            res.setHeader("Content-Type", "text/plain");
-            res.end("Not Found");
-        }
-    }
+  }
 }

fb82620cca1459758b737b8a36f430d9e8409b39
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-04 21:02:11 +0300

    restrructure the routes

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { PipeRouter } from "./core/router";
+// import { PipeRouter } from "./core/router";
 
-const router = new PipeRouter();
+// const router = new PipeRouter();
 
-router.get("/users", () => {
-  console.log("/users");
-});
+// router.get("/users", () => {
+//   console.log("/users");
+// });
 
-router.get("/users/:id", () => {
-  console.log("/users/:id");
-});
+// router.get("/users/:id", () => {
+//   console.log("/users/:id");
+// });
 
-router.use("/api/v1", router);
-router.use("/api/v2", router);
+// router.use("/api/v1", router);
+// router.use("/api/v2", router);
 
-router
-  .route("/some/:id")
-  .get(() => {})
-  .delete(() => {})
-  .patch(() => {});
+// router
+//   .route("/some/:id")
+//   .get(() => {})
+//   .delete(() => {})
+//   .patch(() => {});
 
-console.dir(router, { depth: null });
+// console.dir(router, { depth: null });
 
-console.log(router.collectRoutes(router));
+// console.log(router.collectRoutes(router));

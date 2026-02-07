fc3411b59e1995666ce97ee583cf1502d1d1646d
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-07 16:03:11 +0300

    refactor: comment out unused imports and router setup in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { Request } from "./core/request";
-import { Response } from "./core/response";
-import { PipeRouter } from "./core/router";
-import { PipeServer } from "./core/server";
+// import { Request } from "./core/request";
+// import { Response } from "./core/response";
+// import { PipeRouter } from "./core/router";
+// import { PipeServer } from "./core/server";
 
-export { PipeServer } from "./core/server";
+// export { PipeServer } from "./core/server";
 
-const userRouter = new PipeRouter();
+// const userRouter = new PipeRouter();
 
-userRouter.get("/users", (eq: Request, res: Response) => {
-  res.json({ users: [{ name: "Hluf" }] });
-});
+// userRouter.get("/users", (eq: Request, res: Response) => {
+//   res.json({ users: [{ name: "Hluf" }] });
+// });
 
-userRouter.get("/users/:id", (req: Request, res: Response) => {
-  return res.json({ userId: req.params.id });
-});
+// userRouter.get("/users/:id", (req: Request, res: Response) => {
+//   return res.json({ userId: req.params.id });
+// });
 
-const app = new PipeServer();
+// const app = new PipeServer();
 
-app.use("/api", userRouter);
+// app.use("/api", userRouter);
 
-app.listen(3001, () => {
-  console.log("server is running on port 3000");
-});
+// app.listen(3001, () => {
+//   console.log("server is running on port 3000");
+// });

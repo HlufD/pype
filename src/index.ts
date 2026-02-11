d9ffc153807b8fa037e4f71f03286161935569dd
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-11 21:53:00 +0300

    refactor: add someRouter for handling "/some" route and adjust middleware usage

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,18 @@ export { PipeServer } from "./core/server";
 
 const userRouter = new PipeRouter();
 
+const someRouter = new PipeRouter();
+
 userRouter.get("/users", (eq: Request, res: Response) => {
   res.json({ users: [{ name: "John" }] });
 });
 
 userRouter.get("/users/:id", (req: Request, res: Response) => {
   return res.json({ userId: req.params.id });
+});
+
+someRouter.get("/some/route", (req: Request, res: Response) => {
+  return res.json({ message: "This is from the /some route" });
 });
 
 const app = new PipeServer();
@@ -24,11 +30,13 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 });
 
 app.use("/api", userRouter);
+app.use("/", someRouter);
 
-app.use("/some", (req: Request, res: Response, next: NextFunction) => {});
+app.use("/some", (req: Request, res: Response, next: NextFunction) => {
+  console.log("This is from middleware 2");
+  next();
+});
 
 app.listen(3001, () => {
   console.log("server is running on port 3001");
 });
-
-console.dir(app, { depth: null });

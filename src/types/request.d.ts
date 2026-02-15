032beea9e801957b6199731338d2d56c81b45a0c
Hluf Abebe <hlufabebe2015@gmail.com>
2026-02-15 11:21:43 +0300

    include all properies and method on express

--- src/types/request.d.ts
+++ src/types/request.d.ts
@@ -5,7 +5,49 @@ export interface Request extends IncomingMessage {
 
   query?: Record<string, string | string[]>;
 
-  body: Record<string, any>;
+  body?: Record<string, any>;
+
+  path?: string;
+
+  hostname?: string;
+
+  protocol?: string;
+
+  secure?: boolean;
+
+  ip?: string;
+
+  ips?: string[];
+
+  cookies?: Record<string, string>;
+
+  signedCookies?: Record<string, string>;
+
+  originalUrl?: string;
+
+  baseUrl?: string;
+
+  fresh?: boolean;
+
+  stale?: boolean;
+
+  xhr?: boolean;
+
+  route?: any;
 
   get(header: string): string | string[] | undefined;
+
+  header(header: string): string | string[] | undefined;
+
+  accepts(...types: string[]): string | false;
+
+  acceptsCharsets(...charsets: string[]): string | false;
+
+  acceptsEncodings(...encodings: string[]): string | false;
+
+  acceptsLanguages(...langs: string[]): string | false;
+
+  is(type: string): boolean;
+
+  param(name: string, defaultValue?: string): string | undefined;
 }
